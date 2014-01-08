bf67c644d6977608e4d6f2d9664198d2c84d75d5
Joel Dare <jdare@ksl.com>
2014-01-08 07:07:31 -0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Verify the data
+    $required = array('from','email');
+    
+    // Make sure none of the required fields are blank
+    foreach($required as $r) {
+        if( empty($_REQUEST[$r]) ) {
+            die('ERROR: Invalid data.');
+        }
+    }
+    
+    // Setup the CSV line
+    $line = $_REQUEST['from'] . ',' . $_REQUEST['email'];
+    
+    // Write this email
+    file_put_contents('users.csv', $line);
+    
+    // Show the output
+    echo "SAVED";
+
+?>
