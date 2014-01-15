47b08542996be0bf75876025dfaccf5206f95be4
Joel Dare <jdare@ksl.com>
2014-01-15 07:03:25 -0700

    Added the OS option.

--- index.php
+++ index.php
@@ -11,10 +11,18 @@
     }
     
     // Setup the CSV line
-    $line = DATE('Y-m-d') . ',' . $_REQUEST['from'] . ',' . $_REQUEST['email'] . "\n";
+    $line = DATE('Y-m-d') . ',' . $_REQUEST['from'] . ',' . $_REQUEST['os'] . ',' . $_REQUEST['email'] . "\n";
     
-    // Write this email
+    // Write this email to the CSV
     file_put_contents('users.csv', $line, FILE_APPEND);
+    
+    // Compose an email
+    /*
+    $message = file_get_contents($_REQUEST['from'] . '.txt');
+    if (!empty($message)) {
+        mail($_REQUEST['email'], $_REQUEST['from'] . ' Registration', $message);
+    }
+    */
     
     // Show the output
     echo "SAVED";
